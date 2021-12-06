9aa3ffb80c06f85e8822e82e3fed273e9e468fc7
Sirna Tafese <sirnatafese@Sirnas-Air.hitronhub.home>
2021-12-05 23:20:27 -0600

    feat: Add frequency function with specific range.

--- FrequencyCounter.java
+++ FrequencyCounter.java
@@ -18,87 +18,112 @@ import java.text.NumberFormat;
 public class FrequencyCounter {
 
     public static void main(String[] args) throws IOException{
-    // ***** constants *******
-        
-    final int Max = 1000;               //Amount of numbers in array
-    
-    // ***** variables *****
+        // ***** constants *******
 
-    int count =0;                       //counter for while loop
-    
-    int [] Nums = new int [Max];        //array for the numbers
-    
+        final int Max = 1000;               //Amount of numbers in array
+        final int FI50 = 50;               //final int for 50
+
+        // ***** variables *****
+
+        int count =0;                       //counter for while loop
+
+        int [] nums = new int [Max];        //array for the numbers
+        int [] fq = new int [FI50]; 
+
+        String prompt1 = "";       // asking for any frequneces
+
+        int SpecificRangeNum = 0;     // The variable that stores input prompt 1
+        int sn = 0;                   // specific number user would like to find the fq on
+
         String banner = "";             // output banner
         String prompt = "";             // prompt for user input
-        
+
         String strin = "";              // string fro keyboard input
         String strout = "";             // string for formatted output
-        
+
         String delim = "[ ]+";          // delimiter for splitting input records
         String[] tokens = null;         // used to split input records
-        
+
         // a new line character that works on every computer system
         String nl = System.lineSeparator();
-    
-    // ***** objects *****
-    
-        //Scanner scanner = new Scanner(System.in);
+
+        // ***** objects *****
+
+        Scanner scanner = new Scanner(System.in);                     // connecting to keyboard
         //NumberFormat currency = NumberFormat.getCurrencyInstance();
-        
+
         // file io buffers for reading and writing to text files
-        
+
         //Importing the random number file
         BufferedReader fin = new BufferedReader(new FileReader("RandomNumbersFile.txt"));
-        
+
         //PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter("outfile.txt")));
-    
-    // ***** print banners *****
-    
+
+        // ***** print banners *****
+
         banner = "*****************************" + nl;
         banner += "Name:        Sirna Tafese" + nl;
         banner += "Class:       CS30S" + nl;
         banner += "Assignment:  A1.5" + nl;
         banner += "*****************************" + nl + nl;
-        
+
         System.out.println(banner);
         //fout.print(banner);
-    
-    // ***** Get Input *****
-    
+
+        // ***** Get Input *****
+
         // prompt for input
         // read input from keyboard
         // echo input back to console window
-    
-    // ***** Main Processing *****
-    
-    //set control variable
-    strin = fin.readLine();
-    
-    while(strin != null){
-            //System.out.println(strin);
 
+        // ***** Main Processing *****
+
+        // Set strin (control variable) to the next item in the buffer; this is run only once
+        // check that strin is not null; our stopping condition
+        // update the control variable with the next item in the buffer
+        for (strin = fin.readLine();strin != null;strin = fin.readLine()) {
             //Loading the array
-            Nums[count] = Integer.parseInt(strin);
-            
+            nums[count] = Integer.parseInt(strin);
+
             //Incrementing counter
             count++;
-            
-            //update control variable
-            strin = fin.readLine();
-        } //end of eof  loop
-    
-    // ***** Print Formatted Output *****
-    
-    // ***** Closing Message *****
-    
+        }
+
+        for(int i = 0; i < count; i++){
+            fq[nums[i] - 1]++;
+        }//end of for loop
+
+        for(int i = 0; i < FI50; i++){
+
+            System.out.println(i+1+ ":"+fq[i]);
+
+        }//end of for loop
+
+        
+        //asking user for any ranges to see
+        prompt1 = "Would you like to see any fq of any number specifically, N for next question "; 
+
+        System.out.println(prompt1 + nl + nl);
+        SpecificRangeNum = scanner.nextInt();
+
+        for(int i = 0; i < FI50; i++){
+            sn = fq[SpecificRangeNum - 1];
+        }
+        //printing out specific range number
+        System.out.println(SpecificRangeNum + " had a freq of " + sn + nl + nl);
+
+        // ***** Print Formatted Output *****
+
+        // ***** Closing Message *****
+
         System.out.println();
         System.out.println("end of processing");
         //fout.println("End of Processing");
-        
-    // **** close io buffers *****
-    
+
+        // **** close io buffers *****
+
         //fin.close();
         //fout.close();
     } // end main 
-    
-} // end FormatTemplate
+
+} // end A1.5
